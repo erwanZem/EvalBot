cc0270fd16e7e3993e8cb13b55e26248b885c56f
erwanZem <zemouripro4@gmail.com>
2021-11-29 00:07:43 +0100

    Versio final
    manque certain commentaire sur les fcts

--- Main.s
+++ Main.s
@@ -100,10 +100,10 @@ DUREE   			EQU     0x002FFFFF
 __main	
 
 ;activation des port D,F et E (SWITCH,LED et BUMPER)
-		ldr r6, = SYSCTL_PERIPH_GPIOF  		
-        mov r0, #0x00000058  	;58 pour Port D,F,E			
+		ldr 	r6, = SYSCTL_PERIPH_GPIOF  		
+        mov 	r0, #0x00000038  	;58 pour Port D,F,E			
 
-	    str r0, [r6]
+	    str 	r0, [r6]
 		  
 		nop	   									
 		nop	   
@@ -111,148 +111,197 @@ __main
 	
 ;CONFIGURATION DES LEDS
 ;Configuration des leds en Output
-		ldr r6, = GPIO_PORTF_BASE+GPIO_O_DIR  
-		ldr r0, = PORT45	
-		str r0, [r6]
+		ldr 	r6, = GPIO_PORTF_BASE+GPIO_O_DIR  
+		ldr 	r0, = PORT45	
+		str 	r0, [r6]
   
    ;Enable Digital Function 	
 
-		ldr r6, = GPIO_PORTF_BASE+GPIO_O_DEN	
-		ldr r0, = PORT45 		
-		str r0, [r6]
+		ldr 	r6, = GPIO_PORTF_BASE+GPIO_O_DEN	
+		ldr 	r0, = PORT45 		
+		str 	r0, [r6]
 				
 	;Choix de l'intensite de sortie (2mA)	
 
-		ldr r6, = GPIO_PORTF_BASE+GPIO_O_DR2R	
-        ldr r0, = PORT45		
-		str r0, [r6]
+		ldr 	r6, = GPIO_PORTF_BASE+GPIO_O_DR2R	
+        ldr 	r0, = PORT45		
+		str 	r0, [r6]
 		
 	; Enable Digital Function - Port E
 
-		ldr r7, = GPIO_PORTE_BASE+GPIO_O_DEN	
-        ldr r0, = PORT01		
-        str r0, [r7]	
+		ldr 	r7, = GPIO_PORTE_BASE+GPIO_O_DEN	
+        ldr 	r0, = PORT01		
+        str 	r0, [r7]	
 	; Activer le registre des bumpers, Port E
 
-		ldr r7, = GPIO_PORTE_BASE+GPIO_PUR	
-        ldr r0, = PORT01
-        str r0, [r7]
+		ldr 	r7, = GPIO_PORTE_BASE+GPIO_PUR	
+        ldr 	r0, = PORT01
+        str 	r0, [r7]
 		
 	;Activation du port d
-		ldr r6, = GPIO_PORTD_BASE+GPIO_O_DEN	;;Enable Digital Function (p316 )
-        ldr r0, = PORT6 + PORT7		
-        str r0, [r6]
+		ldr 	r6, = GPIO_PORTD_BASE+GPIO_O_DEN	;;Enable Digital Function (p316 )
+        ldr 	r0, = PORT6 + PORT7		
+        str 	r0, [r6]
 
 	;Activation des switchs
-		ldr r6, = GPIO_PORTD_BASE+GPIO_PUR	;; Choix de l'intensité de sortie (2mA)
-        ldr r0, = PORT6 + PORT7			
-        str r0, [r6]
+		ldr 	r6, = GPIO_PORTD_BASE+GPIO_PUR	;; Choix de l'intensité de sortie (2mA)
+        ldr 	r0, = PORT6 + PORT7			
+        str		r0, [r6]
 		
 		
 	;Activation des moteurs
-		B	MOTEUR_INIT
-		B	MOTEUR_DROIT_ON
-		B	MOTEUR_GAUCHE_ON
+		BL		MOTEUR_INIT
+		BL		MOTEUR_DROIT_ON
+		BL		MOTEUR_GAUCHE_ON
 		
 		;FIN CONFIGURATION
 		
 		;BOUCLE PRINCIPALE
 loop	
-		B AVANCER
+		BL 		AVANCER
 		
-		B LIRE_BUMPER_DROIT
+		BL 		LIRE_BUMPER_DROIT
 		
-		cmp	r5,#0x00
+		cmp		r5,#0x00
 		
-		BNE DEMI_TOUR_DROIT
+		BEQ 	DEMI_TOUR_DROIT
 		
-		B LIRE_BUMPER_GAUCHE
+		BL 		LIRE_BUMPER_GAUCHE
 		
-		CMP r9,#0x00
+		CMP 	r10,#0x00
 		
-		BNE DEMI_TOUR_GAUCHE
+		BEQ 	DEMI_TOUR_GAUCHE
+
+		BL 		LIRE_SWITCH1
 		
-		B loop
+		cmp		r5,#0x00
+		
+		BEQ 	BONUS
+
+		BL 		LIRE_SWITCH2
+		
+		CMP 	r10,#0x00
+		
+		BEQ 	BONUS
+		
+		BL		loop
 		;FIN DE LA BOUCLE
 
 		
 		;FONCTIONS UTILS
 LIRE_BUMPER_DROIT
-	
-		ldr r7,= GPIO_PORTE_BASE + (PORT0<<2)
-		ldr r5, [r7]
-		BX	LR
+		;Enregistrement de la valeur a l'adresse mémoire du port broche 0 dans le registre r5
+		ldr 	r7,= GPIO_PORTE_BASE + (PORT0<<2)
+		ldr 	r5, [r7]
+		BX		LR
 		
 LIRE_BUMPER_GAUCHE
+		;Enregistrement de la valeur a l'adresse mémoire du port broche 1 dans le registre r10
 
-        ldr r9, =  GPIO_PORTE_BASE + (PORT1<<2)
-		ldr r10, [r9]
-		BX	LR
+        ldr 	r9, =  GPIO_PORTE_BASE + (PORT1<<2)
+		ldr 	r10, [r9]
+		BX		LR
 		
 AVANCER
-		mov r12,LR
-		b MOTEUR_DROIT_AVANT
-		b MOTEUR_GAUCHE_AVANT
-		BX	r12
+;Active les moteurs vers l'avant
+		mov 	r12,LR
+		BL		MOTEUR_DROIT_AVANT
+		BL 		MOTEUR_GAUCHE_AVANT
+		BX		r12
 		
 RECULER
-		mov r12,LR
-		b MOTEUR_DROIT_ARRIERE
-		b MOTEUR_GAUCHE_ARRIERE
-		BX	r12
+;Activer les moteur vers l'arriere
+		mov 	r10,LR
+		BL 		MOTEUR_DROIT_ARRIERE
+		BL		MOTEUR_GAUCHE_ARRIERE
+		BX		r10
 		
 DEMI_TOUR_DROIT
-		mov r12,LR	
-		b MOTEUR_DROIT_ARRIERE
-		b MOTEUR_GAUCHE_ARRIERE
-		b WAIT
-		b MOTEUR_DROIT_ON
-		nop
-		BX	r12
+			; Evalbot recule
+		BL 		RECULER
+		; Recule pendant 4 wait
+		BL 		CLIGNOTER
+		; Rotation dde l'Evalbot pendant une demi-période (1 seul WAIT)
+		BL		MOTEUR_GAUCHE_AVANT
+		BL		MOTEUR_DROIT_ARRIERE
+
+		BL 		CLIGNOTER
+		BL		loop
 
 
+DEMI_TOUR_GAUCHE	
+			; 
+		BL		RECULER
+
+     	; Recule pendant 1 wait
+		BL 		CLIGNOTER	
+		; Rotation dde l'Evalbot pendant une demi-période (1 seul WAIT)
+		BL		MOTEUR_GAUCHE_ARRIERE
+		BL		MOTEUR_DROIT_AVANT
+		BL 		CLIGNOTER
+	    BL 		loop
+		
+		
+		
 ; pour la durée de la boucle d'attente1 (wait1)
 WAIT 	
-		ldr r1, = DUREE
-w1		subs r1, #1
-		bne w1
-		BX	LR
+		ldr 	r1, = DUREE
+w1		subs 	r1, #1 	
+		bne 	w1
+		BX		LR
 		
+ALLUMER_LED
+	mov 	r3, #(PORT4 + PORT5)
+	ldr 	r6, = GPIO_PORTF_BASE+ ((PORT4+PORT5)<<2) ;
+	str 	r3, [r6]
+	BX		LR
+
+CLIGNOTER
+	mov		r10,LR
+	BL		ALLUMER_LED
+	BL		WAIT
+	BL		ETEINDRE_LED
+	BL	  	WAIT
+	BL		ALLUMER_LED
+	BL		WAIT
+	BL		ETEINDRE_LED
+	BL 		WAIT
+	BL		ALLUMER_LED
+	BL		WAIT
+	BL		ETEINDRE_LED
+	BX		r10
+
+
+
+ETEINDRE_LED
+	mov 	r2,#0x00
+	ldr 	r6, = GPIO_PORTF_BASE+ ((PORT4+PORT5)<<2) ;
+	str 	r2, [r6]
+	BX		LR
+	
 LIRE_SWITCH1
-		ldr r7, = GPIO_PORTD_BASE+ (PORT6<<2)
-		ldr r5, [r7]
-		BX LR
+	ldr 	r7, = GPIO_PORTD_BASE+ (PORT6<<2)
+	ldr 	r5, [r7]
+	BX 		LR
 
 LIRE_SWITCH2
-		ldr r9, = GPIO_PORTD_BASE+ (PORT7<<2)
-		ldr r10, [r9]
-		BX LR
+	ldr 	r9, = GPIO_PORTD_BASE+ (PORT7<<2)
+	ldr 	r10, [r9]
+	BX		LR
 		
-;DEMI_TOUR_GAUCHE
-	;BX	LR
 
-;ARRET
-		;b MOTEUR_GAUCHE_OFF
-		;b MOTEUR_DROIT_OFF
 		
-		;BX	LR
-		
-;CLIGNOTE
-;	BX	LR
 
-;BONUS
-;BEYBLADE
-;; Rotation à droite de l'Evalbot pendant une demi-période (1 seul WAIT)
-		;CLIGNOTE
-;		B	MOTEUR_DROIT_ARRIERE   ; 
-;		B	MOTEUR_DROIT_INVERSE
-;		BL	WAIT
-;
-;		b	loop
-;BX	LR
+BONUS
+	mov 	r12,LR
+	BL		MOTEUR_DROIT_ARRIERE   ; 
+	BL		MOTEUR_GAUCHE_AVANT
+	BL		CLIGNOTER	
+	BL		loop
+
 
 	nop
 	nop
 	nop
 
-	END
+END
